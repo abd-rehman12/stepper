c3a5653d6cd6db94280c755a8f321b6c1b545156
abd-rehman12 <117512314+abd-rehman12@users.noreply.github.com>
2024-08-20 17:01:52 +0500

    updated slice

--- src/Features/Slice/signUpSlice.js
+++ src/Features/Slice/signUpSlice.js
@@ -15,35 +15,33 @@ const signUpSlice = createSlice({
   initialState,
   reducers: {
     addFirstName: (state, action) => {
-        const info={
-            firstName:action.payload
-        };
-      state.info.firstName=info.firstName;
-      console.log("from slice", info.firstName, action.payload  );
+        
+      state.info.firstName=action.payload;
+      // console.log("from slice", info.firstName, action.payload  );
       localStorage.setItem("info", JSON.stringify(state.info)); // Save to localStorage
     },
     addLastName: (state, action) => {
-        const info={
-            lastName:action.payload
-        };
-        state.info.lastName=info.lastName;
-      console.log("from slice", info.lastName, action.payload  );
+        // const info={
+        //     lastName:
+        // };
+        state.info.lastName=action.payload;
+      // console.log("from slice", info.lastName, action.payload  );
       localStorage.setItem("info", JSON.stringify(state.info)); // Save to localStorage
     },
     addPhoneNumber: (state, action) => {
-      const info={
-          PhoneNum:action.payload
-      };
-      state.info.PhoneNum=info.PhoneNum;
-    console.log("from slice", info.PhoneNum, action.payload  );
+      // const info={
+      //     PhoneNum:action.payload
+      // };
+      state.info.PhoneNum=action.payload;
+    // console.log("from slice", info.PhoneNum, action.payload  );
     localStorage.setItem("info", JSON.stringify(state.info)); // Save to localStorage
   },
   addEmail: (state, action) => {
-    const info={
-        Email:action.payload
-    };
-    state.info.Email=info.Email;
-  console.log("from slice", info.Email, action.payload  );
+    // const info={
+    //     Email:action.payload
+    // };
+    state.info.Email=action.payload;
+  // console.log("from slice", info.Email, action.payload  );
   localStorage.setItem("info", JSON.stringify(state.info)); // Save to localStorage
 },
   },
